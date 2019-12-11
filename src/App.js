9d93ddbd617abf6626ee34728ce972e4315bc491
Eian Carter <eian@bricleir.com>
2019-12-11 15:38:34 -0800

    revised styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import PhotoList from "./components/PhotoList";
+import NavigationList from "./components/NavigationList";
+import { Button } from "reactstrap";
 import "./styles/styles.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="Nav">
+       <NavigationList/>
+      </div>
+      <h1>NASA Photo of the Day</h1>
       <PhotoList/>
     </div>
   );
---
+++ src/components/NavigationList.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText} from "reactstrap";
+
+export default function NavigationList(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+    // <div className="Nav">  
+        <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">NASA POTD</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/components/">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/components/">Donate</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/components/">Contact</NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavBar>
+                        <DropdownToggle nav caret>
+                            Past Photos
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem>
+                                Yesterday
+                            </DropdownItem>
+                            <DropdownItem>
+                                December 9, 2019
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem>
+                                Show More
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+                <NavbarText>Welcome!</NavbarText>
+            </Collapse>
+        </Navbar>
+    // </div>
+    )
+}
